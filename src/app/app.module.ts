0f56cfbbe821011a4bcfa0386a5cd20bdd1a269b
Steve <strong_stephen@bah.com>
2018-07-14 16:25:01 -0400

    toast works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { ToastrModule } from 'ngx-toastr';
+
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    NgbModule.forRoot(),
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [NgbModule.forRoot(), ToastrModule.forRoot(), BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
