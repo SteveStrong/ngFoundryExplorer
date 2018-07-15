9bdce6edace5accc29d3057d381d4477d03aeb5d
Steve <strong_stephen@bah.com>
2018-07-15 11:22:31 -0400

    prepare for boid

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,8 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { WelcomeComponent } from './welcome/welcome.component';
 
+import { BoidstestComponent } from './boidstest/boidstest.component';
+
 const routes: Routes = [
+  { path: 'boid', component: BoidstestComponent }
   { path: 'welcome', component: WelcomeComponent }
+  { path: '', pathMatch: 'full', redirectTo: 'boid' },
 ];
 
 @NgModule({
