68dfa747f69d4c427cbf053057ccb4a9751895c4
Steve <strong_stephen@bah.com>
2018-07-15 09:54:17 -0400

    new nav

---
+++ src/app/boidstest/boidstest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boidstest',
+  templateUrl: './boidstest.component.html',
+  styleUrls: ['./boidstest.component.css']
+})
+export class BoidstestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
