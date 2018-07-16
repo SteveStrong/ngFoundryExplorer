564e8a33f47db251caef685a85bae57c994ef238
Steve <strong_stephen@bah.com>
2018-07-15 23:41:26 -0400

    it compiles

--- projects/ng-foundry-models/src/public_api.ts
+++ projects/ng-foundry-models/src/public_api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of ng-foundry-models
  */
 
+export * from './lib/emitter.service';
+
 export * from './lib/ng-foundry-models.service';
 export * from './lib/ng-foundry-models.component';
 export * from './lib/ng-foundry-models.module';
@@ -27,4 +29,23 @@ export * from './lib/models/foSolution.model';
 
 // export * from './lib/models/foWorkspace.model';
 
+
+export * from './lib/models/shapes/canvasDriver';
+
+export * from './lib/models/shapes/foConnectionPoint2D';
+export * from './lib/models/shapes/foDisplay2D.model';
+export * from './lib/models/shapes/foDocument.model';
+export * from './lib/models/shapes/foGeometry2D';
+export * from './lib/models/shapes/foGlue2D';
+export * from './lib/models/shapes/foLayout2D';
+export * from './lib/models/shapes/foGlyph2D.model';
+export * from './lib/models/shapes/foHandle2D';
+export * from './lib/models/shapes/foImage2D.model';
+export * from './lib/models/shapes/foMatrix2D';
+export * from './lib/models/shapes/foPage.model';
+export * from './lib/models/shapes/foPath2D.model';
+export * from './lib/models/shapes/foPathSVG';
+export * from './lib/models/shapes/foShape1D.model';
+export * from './lib/models/shapes/foShape2D.model';
+export * from './lib/models/shapes/foText2D.model';
 
