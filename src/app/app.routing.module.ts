5de5fd1bd940915d9f2c18296f4a574e6bb4d28e
Steve <strong_stephen@bah.com>
2018-07-22 13:02:50 -0400

    render other components

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { WelcomeComponent } from './welcome/welcome.component';
-
 import { BoidstestComponent } from './boidstest/boidstest.component';
+import { CanvastestComponent } from './canvastest/canvastest.component';
+import { ShapetestComponent } from './shapetest/shapetest.component';
 
 const routes: Routes = [
+  { path: 'canvas', component: CanvastestComponent },
+  { path: 'shape', component: ShapetestComponent },
   { path: 'boid', component: BoidstestComponent },
   { path: 'welcome', component: WelcomeComponent },
   { path: '', pathMatch: 'full', redirectTo: 'boid' }
